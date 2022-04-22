b427861de785382a2e80c1bdfd3a51eb6500ff5e
Antonio Mancera <antoniomanuelmancerahevilla@gmail.com>
2022-04-22 23:19:34 +0200

    style: change word methods

---
+++ sample/letter.py
@@ -0,0 +1,20 @@
+class Letter:
+    def __init__(self, chr,pos,count,more):
+        self.chr = chr
+        self.pos = pos
+        self.count = count
+        self.more = more
+
+    def valoresFijos(self):
+        total=0
+        for i in self.pos:
+            if(i==1):
+                total+=1
+        return total
+            
+
+    def print(self):
+        print("la letra: ",self.chr," aparece: ", self.count, " veces, en las posiciones: "
+        , self.pos)
+    
+    
